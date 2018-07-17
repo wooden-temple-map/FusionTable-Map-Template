8c73c2bd9d74031c182e63987fdac08428551acf
wooden-temple-map <33306414+wooden-temple-map@users.noreply.github.com>
2018-07-17 12:10:12 +0300

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -9,11 +9,11 @@
         this.searchRadius = options.searchRadius || 805; //in meters ~ 1/2 mile
 
         // the encrypted Table ID of your Fusion Table (found under File => About)
-        this.fusionTableId = options.fusionTableId || "",
+        this.fusionTableId = options.fusionTableId || "1z1G-QjG97ffSRaguoZA08t2HenzE6NB99ZdMfekF",
 
         // Found at https://console.developers.google.com/
         // Important! this key is for demonstration purposes. please register your own.
-        this.googleApiKey = options.googleApiKey || "",
+        this.googleApiKey = options.googleApiKey || "AIzaSyCrm7YDdpPU7BBPpyyGgdbQasxU2WRHVM8",
         
         // name of the location column in your Fusion Table.
         // NOTE: if your location column name has spaces in it, surround it with single quotes
